a633fab410524c187cba6d93560ccd2926b9eb14
Romil Shah <romilshah2199@gmail.com>
2023-10-17 12:14:18 +0530

    add initial styles and cleanup

---
+++ src/components/Song.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Song = () => {
+  return (
+    <div className="song-container">
+      <p>Song Image</p>
+      <p>Song Name</p>
+      <p>Author Name</p>
+    </div>
+  );
+};
+
+export default Song;
---
+++ src/components/SongPlayer.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faPlay,
+  faAngleLeft,
+  faAngleRight,
+} from "@fortawesome/free-solid-svg-icons";
+
+const SongPlayer = () => {
+  return (
+    <div className="player-container">
+      <div className="time-control">
+        <p>Start Time</p>
+        <input type="range" />
+        <p>End Time</p>
+      </div>
+
+      <div className="player-control">
+        <FontAwesomeIcon className="skip-back" icon={faAngleLeft} size="2x" />
+        <FontAwesomeIcon className="play" icon={faPlay} size="2x" />
+        <FontAwesomeIcon
+          className="skip-forward"
+          icon={faAngleRight}
+          size="2x"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default SongPlayer;
